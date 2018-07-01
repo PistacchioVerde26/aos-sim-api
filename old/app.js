8905a10ec2fa7fbfbf82d73611ba89fa9063f5af
Shapeinabox <shapeinabox@gmail.com>
2018-07-01 20:14:51 +0200

    port config for heroku

--- old/app.js
+++ old/app.js
@@ -12,7 +12,7 @@ const { Miscs } = require('./models/Miscs');
 
 var app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // app.use(cors())
 app.use(bodyParser.json());
