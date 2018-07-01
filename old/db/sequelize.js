67a7623229cc3952368b2848475073837571c0ee
Shapeinabox <shapeinabox@gmail.com>
2018-07-01 20:24:13 +0200

    db config for heroku

--- old/db/sequelize.js
+++ old/db/sequelize.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 
-const sequelize = new Sequelize('aos_db_testing', 'root', '', {
+const sequelize = new Sequelize('lvz74x42v5w3aong', 'bpkpenhzi0brtsrd', 'p64svbslxikvsnzm', {
+    host: '	jsftj8ez0cevjz8v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     dialect: 'mysql',
     logging: true
 });
