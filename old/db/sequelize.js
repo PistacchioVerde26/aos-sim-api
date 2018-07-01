aba2944688a7d1939876c7cf430230f36e6184e6
Shapeinabox <shapeinabox@gmail.com>
2018-07-01 20:27:12 +0200

    db config for heroku

--- old/db/sequelize.js
+++ old/db/sequelize.js
@@ -1,10 +1,8 @@
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 
-const sequelize = new Sequelize('lvz74x42v5w3aong', 'bpkpenhzi0brtsrd', 'p64svbslxikvsnzm', {
-    host: '	jsftj8ez0cevjz8v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    dialect: 'mysql',
-    logging: true
+const sequelize = new Sequelize('mysql://bpkpenhzi0brtsrd:p64svbslxikvsnzm@jsftj8ez0cevjz8v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/lvz74x42v5w3aong', {
+    logging: false
 });
 
 module.exports = { sequelize, Op }
